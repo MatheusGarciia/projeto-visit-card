f82ed7e12db612916a03e2b16ce626d54a9049e6
Matheus01~ <KoringAnonimo@gmail.com>
2023-07-20 16:12:50 -0300

    :tada: initial commit 1~

---
+++ script.js
@@ -0,0 +1,21 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  // substituir a imagem
+  if (html.classList.contains("light")) {
+    // se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("alt", "Luffy olhando para o mar")
+  } else {
+    // se tiver sem light mode, manter a imagem normal
+    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute(
+      "alt",
+      "Foto de Matheus Garcia, usando camisa branca e o cabelo longo, com fundo em sua casa."
+    )
+  }
+}
