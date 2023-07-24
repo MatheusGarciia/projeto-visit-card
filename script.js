76ac1705f057a3c22016d49c5d6143a80686addb
Matheus01~ <KoringAnonimo@gmail.com>
2023-07-24 15:38:09 -0300

    avatar change

--- script.js
+++ script.js
@@ -8,14 +8,14 @@ function toggleMode() {
   // substituir a imagem
   if (html.classList.contains("light")) {
     // se tiver light mode, adicionar a imagem light
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./assets/Avatar1.png")
     img.setAttribute(
       "alt",
       "Foto de Matheus Garcia, usando camisa branca e o cabelo longo, com fundo em sua casa."
     )
   } else {
     // se tiver sem light mode, manter a imagem normal
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./assets/Avatar1.png")
     img.setAttribute(
       "alt",
       "Foto de Matheus Garcia, usando camisa branca e o cabelo longo, com fundo em sua casa."
